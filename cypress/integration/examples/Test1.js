71731bf634f512086d01370de6f59266eb4f6bf5
Affan Sayeed <affansayeed234@gmail.com>
2025-08-23 22:33:38 +0530

    first commit

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,32 @@
+// cypress - Spec
+
+// cy is the cypress object and cy is a global variable
+/// <reference types="Cypress" />
+
+describe("My First Test", function () {
+  it("My First Test case", function () {
+    // test step gp here
+
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/"); // to open a website
+    cy.get(".search-keyword").type("ca");
+    cy.wait(2000);
+    // cy.get(".product:visible").should("have.length", 4);
+
+    // Parent child chainning
+    cy.get(".products").find(".product").should("have.length", 4);
+    // cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
+    cy.get(".products")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const textVeg = $el.find("h4.product-name").text();
+        if (textVeg.includes("Cashews")) {
+          cy.wrap($el).find("button[type=button]").click();
+          // $el.find("button[type=button]").trigger("click"); // it also works
+        }
+      });
+  });
+
+  //   it("My Second Test case", function () {
+  //     // test step gp here
+  //   });
+});
