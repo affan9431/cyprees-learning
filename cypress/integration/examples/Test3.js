0234cd6c2a9e68282a13479e6c493425af9589b4
Affan Sayeed <affansayeed234@gmail.com>
2025-10-04 11:28:36 +0530

    Practice more test cases by writing more code in Test3.js file

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -15,5 +15,15 @@ describe("My Third Test", function () {
       .and("have.value", "option1");
     cy.get("#checkBoxOption1").uncheck().should("not.be.checked");
     cy.get("input[type='checkbox']").check(["option2", "option3"]);
+    cy.get("select").select("option2").should("have.value", "option2");
+    cy.get("#autocomplete").type("ind");
+
+    cy.get(".ui-menu-item div").each(($el, index, $list) => {
+      if ($el.text() === "India") {
+        cy.wrap($el).click();
+      }
+    });
+
+    cy.get('[value="radio1"]').check().should("be.checked");
   });
 });
